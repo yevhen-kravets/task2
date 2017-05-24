426278730829d1edc54d4b17221a09f9375f99fd
Yevhen Kravets <krava2108@gmail.com>
2017-05-24 23:02:35 +0300

    fight implementation

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 class Fighter {
-    constructor(name, power, health=100) {
+    constructor(name, power, health = 100) {
         this.name = name;
         this.power = power;
         this.health = health;
     }
 
     setDamage(damage) {
-        this.health = this.health - damage;
-        console.log(`${this.name} health: ${this.health}`);
+        const val = Math.round(this.health - damage);
+        this.health = val < 0 ? 0 : val;
+        console.log(`${this.name}'s health: ${this.health}`);
     }
 
     hit(enemy, point) {
         let damage = point * this.power;
-        enemy.setDamage(damage);
+        if (enemy.health > 0) enemy.setDamage(damage);
     }
 }
 
 class ImprovedFighter extends Fighter {
-    constructor(name, power, health=100) {
-        super(name, power, health);
+    constructor(...props) {
+        super(...props);
     }
 
     doubleHit(enemy, point) {
@@ -26,6 +27,26 @@ class ImprovedFighter extends Fighter {
     }
 }
 
+let fighter = new Fighter('Fighter', 0.4);
+let improvedFighter = new ImprovedFighter('ImprovedFighter', 0.25);
+
 function fight(fighter, improvedFighter, ...points) {
-    
+    console.log('!!new round!!');
+    let loser;
+    points.forEach(point => {
+        fighter.doubleHit ? fighter.doubleHit(improvedFighter, point) : fighter.hit(improvedFighter, point);
+        if (improvedFighter.health === 0) {
+            loser = improvedFighter;
+            return;
+        }
+        improvedFighter.doubleHit ? improvedFighter.doubleHit(fighter, point) : improvedFighter.hit(fighter, point);
+        if (fighter.health === 0) {
+            loser = fighter;
+            return;
+        }
+    });
+    if (!loser) return fight(fighter, improvedFighter, ...points);
+    console.log(`${loser.name} losing battle`);
 }
+
+fight(fighter, improvedFighter, 25, 13, 45);
